9ea29f94dedbbf5b75e4b988ac43dee31590d762
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-05-09 12:09:41 +0300

    Class Title Hw

---
+++ class_titleHW.py
@@ -0,0 +1,9 @@
+class Title:
+    def __init__(self,text):
+        self.text= text
+
+    def __str__(self):
+        return "-= " + self.text.upper() +" =-"
+    
+t1= Title("Programming in Python 3")
+print (t1)
