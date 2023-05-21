e19b5cdc3df555e80ba7718fa41627f3d596435e
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-05-21 16:35:35 +0300

    app.py Updated

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from Node import Node
 from algo import traverse
 
-n1 = Node("1.Solve it peaceful betwen the parts? ")
+n1 = Node("1.Do you want to go to the Sea? ")
 n1.nextYes = Node("2. Succes")
-n1.nextNo = Node("3.Reported and solved by superior? ")
+n1.nextNo = Node("3.Do you want to go to the Mountain? ")
 
 n1.nextNo.nextYes = Node("4. Succces!")
-n1.nextNo.nextNo = Node("5. Solve by law? ")
+n1.nextNo.nextNo = Node("5. Do you want to go to the pool? ")
 
 n1.nextNo.nextNo.nextYes= Node("6.Succes!")
 n1.nextNo.nextNo.nextNo= Node("7.Unsucces!")
